46c5eac4eff051ecca1cc0700d57945650b225ec
George Lydakis <ge0rge.lyd@gmail.com>
2013-03-12 19:14:07 +0200

    Initial Commit

---
+++ SuperMarket/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SuperMarket
+//
+//  Created by George Lydakis on 3/12/13.
+//  Copyright (c) 2013 Brainstorm. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+

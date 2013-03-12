4f7a4e7affea845dba827a900360a368f07b49a1
George Lydakis <ge0rge.lyd@gmail.com>
2013-03-12 20:10:22 +0200

    and even more housekeeping

---
+++ SuperMarket/FileOperations.h
@@ -0,0 +1,14 @@
+//
+//  FileOperations.h
+//  SuperMarket
+//
+//  Created by George Lydakis on 3/12/13.
+//  Copyright (c) 2013 Brainstorm. All rights reserved.
+//
+
+#ifndef SuperMarket_FileOperations_h
+#define SuperMarket_FileOperations_h
+
+
+
+#endif

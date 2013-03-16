fa07194dba238a709b1c74df8b4bbc8d3e04f07a
George Lydakis <ge0rge.lyd@gmail.com>
2013-03-16 23:43:27 +0200

    adding Points files

---
+++ SuperMarket/Points.c
@@ -0,0 +1,10 @@
+//
+//  Points.c
+//  SuperMarket
+//
+//  Created by George Lydakis on 3/16/13.
+//  Copyright (c) 2013 Brainstorm. All rights reserved.
+//
+
+#include <stdio.h>
+#include "Points.h"
---
+++ SuperMarket/Points.h
@@ -0,0 +1,14 @@
+//
+//  Points.h
+//  SuperMarket
+//
+//  Created by George Lydakis on 3/16/13.
+//  Copyright (c) 2013 Brainstorm. All rights reserved.
+//
+
+#ifndef SuperMarket_Points_h
+#define SuperMarket_Points_h
+
+
+
+#endif
